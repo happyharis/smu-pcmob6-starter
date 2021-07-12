cebb1badad9a7e93c82978e8e0ddb4fdc7463b78
Haris Samingan <muhamad_haris@hotmail.com>
2021-07-12 09:57:05 +0800

    Add refresh indicator

--- screens/IndexScreen.js
+++ screens/IndexScreen.js
@@ -1,5 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { Text, View, TouchableOpacity, FlatList, RefreshControl} from "react-native";
+import {
+  Text,
+  View,
+  TouchableOpacity,
+  FlatList,
+  RefreshControl,
+} from "react-native";
 import { FontAwesome } from "@expo/vector-icons";
 import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
@@ -7,16 +13,26 @@ import { API, API_POSTS } from "../constants/API";
 import { lightStyles } from "../styles/commonStyles";
 
 export default function IndexScreen({ navigation, route }) {
-
   const [posts, setPosts] = useState([]);
   const styles = lightStyles;
+  const [refreshing, setRefreshing] = useState(false);
+
+  async function onRefresh() {
+    setRefreshing(true);
+    const response = await getPosts();
+    setRefreshing(false);
+  }
 
   // This is to set up the top right button
   useEffect(() => {
     navigation.setOptions({
       headerRight: () => (
         <TouchableOpacity onPress={addPost}>
-          <FontAwesome name="plus" size={24} style={{ color: styles.headerTint, marginRight: 15 }} />
+          <FontAwesome
+            name="plus"
+            size={24}
+            style={{ color: styles.headerTint, marginRight: 15 }}
+          />
         </TouchableOpacity>
       ),
     });
@@ -42,30 +58,28 @@ export default function IndexScreen({ navigation, route }) {
     try {
       const response = await axios.get(API + API_POSTS, {
         headers: { Authorization: `JWT ${token}` },
-      })
+      });
       console.log(response.data);
       setPosts(response.data);
-      return "completed"
+      return "completed";
     } catch (error) {
       console.log(error.response.data);
-      if (error.response.data.error = "Invalid token") {
+      if ((error.response.data.error = "Invalid token")) {
         navigation.navigate("SignInSignUp");
       }
     }
   }
 
-  function addPost() {
-    
-  }
+  function addPost() {}
 
-  function deletePost() {
-    
-  }
+  function deletePost() {}
 
   // The function to render each row in our FlatList
   function renderItem({ item }) {
     return (
-      <TouchableOpacity onPress={() => navigation.navigate("Details", {post: item})}>
+      <TouchableOpacity
+        onPress={() => navigation.navigate("Details", { post: item })}
+      >
         <View
           style={{
             padding: 10,
@@ -75,7 +89,8 @@ export default function IndexScreen({ navigation, route }) {
             borderBottomWidth: 1,
             flexDirection: "row",
             justifyContent: "space-between",
-          }}>
+          }}
+        >
           <Text style={styles.text}>{item.title}</Text>
           <TouchableOpacity onPress={deletePost}>
             <FontAwesome name="trash" size={20} color="#a80000" />
@@ -92,8 +107,14 @@ export default function IndexScreen({ navigation, route }) {
         renderItem={renderItem}
         style={{ width: "100%" }}
         keyExtractor={(item) => item.id.toString()}
+        refreshControl={
+          <RefreshControl
+            colors={["#9Bd35A", "#689F38"]}
+            refreshing={refreshing}
+            onRefresh={onRefresh}
+          />
+        }
       />
     </View>
   );
 }
-
