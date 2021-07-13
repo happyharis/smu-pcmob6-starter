8537088187c72f585857c310120ea01cafd8ad30
Haris Samingan <muhamad_haris@hotmail.com>
2021-07-14 07:21:49 +0800

    Add pic action

--- redux/ducks/accountPrefs.js
+++ redux/ducks/accountPrefs.js
@@ -1,5 +1,7 @@
 const TOGGLE_DARKMODE = "toggle_darkmode";
 const REST_DARKMODE = "reset_darkmode";
+const UPLOAD_PIC = "upload_pic";
+const DELETE_PIC = "delete_pic";
 
 export function toggleDarkMode() {
   return {
@@ -12,8 +14,17 @@ export function resetDarkMode() {
   };
 }
 
+export function uploadPicAction() {
+  return { type: UPLOAD_PIC };
+}
+
+export function deletePicAction() {
+  return { type: DELETE_PIC };
+}
+
 const initialState = {
   darkMode: false,
+  profilePicture: null,
 };
 
 export default function accontPrefsReducer(state = initialState, action) {
@@ -23,6 +34,11 @@ export default function accontPrefsReducer(state = initialState, action) {
 
     case REST_DARKMODE:
       return { ...state, darkMode: false };
+
+    case UPLOAD_PIC:
+      return { ...state, profilePicture: action.payload };
+    case DELETE_PIC:
+      return { ...state, profilePicture: null };
 
     default:
       return state;
