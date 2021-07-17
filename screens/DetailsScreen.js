4135f92eed7da5d045f4b8c19faa2d87b28a0717
Haris Samingan <muhamad_haris@hotmail.com>
2021-07-17 12:41:45 +0800

    Add details

--- screens/DetailsScreen.js
+++ screens/DetailsScreen.js
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from "react";
 import { Text, View, TouchableOpacity } from "react-native";
 import { FontAwesome } from "@expo/vector-icons";
+import { API_POSTS, API } from "../constants/API";
 import { commonStyles, lightStyles } from "../styles/commonStyles";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import axios from "axios";
 
 export default function ShowScreen({ navigation, route }) {
-  const [post, setPost] = useState(route.params.post);
+  const [post, setPost] = useState({ title: "", content: "" });
   const styles = { ...lightStyles, ...commonStyles };
 
   useEffect(() => {
@@ -22,16 +25,41 @@ export default function ShowScreen({ navigation, route }) {
   });
 
   useEffect(() => {
-    console.log(route.params.post);
+    getPost();
   }, []);
 
+  async function getPost() {
+    const token = await AsyncStorage.getItem("token");
+    const id = route.params.post.id;
+    console.log(id);
+    console.log(token);
+    try {
+      const response = await axios.get(API + API_POSTS + "/" + id, {
+        headers: { Authorization: `JWT ${token}` },
+      });
+      console.log(response.data);
+      setPost(response.data);
+    } catch (error) {
+      console.log(error);
+      // console.log(error.response.data);
+      // if ((error.response.data.error = "Invalid token")) {
+      //   navigation.navigate("SignInSignUp");
+      // }
+    }
+  }
+
   function editPost() {
-    navigation.navigate("Edit");
+    navigation.navigate("Edit", { post: post });
   }
 
   return (
     <View style={styles.container}>
-      <Text>Details Screen</Text>
+      <Text style={[styles.title, styles.text, { margin: 40 }]}>
+        {post.title}
+      </Text>
+      <Text style={[styles.content, styles.text, { margin: 20 }]}>
+        {post.content}
+      </Text>
     </View>
   );
 }
